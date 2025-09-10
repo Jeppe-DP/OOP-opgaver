e282505199cc5fb8560b142e71a637e81cb434ce
Jaevs <jeppedeleuran@yahoo.dk>
2025-09-10 16:33:32 +0200

    opgave 6.3.3

---
+++ opgaver-kap-6/6.3.3/Program.cs
@@ -0,0 +1,10 @@
+﻿int var = 2000000;
+double price = 599.95;
+double current_price = price;
+
+//Determines the price during christmas
+if(var < 31104000-86400*11*30 && var > 6*86400) {
+    current_price=price*0.7;
+}
+
+Console.WriteLine("the current price is " + current_price);
