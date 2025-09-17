9dd3fc1047b2938a828e55d7d92e74c8d96f8422
jeppe-dp <jeppedeleuran@yahoo.dk>
2025-09-17 13:59:31 +0200

    kapitel 8

--- opgaver-kap-7/7.6.12/Program.cs
+++ opgaver-kap-7/7.6.12/Program.cs
@@ -12,7 +12,8 @@ for (int i = 2000; i < 2021; i++){
     else {
         pointer = days_in_month;
     }
-    foreach (int day in pointer){
-    Console.WriteLine(day);
-    }
+        foreach (int day in pointer){
+        Console.Write(day+ " ");
+        }
+    Console.WriteLine("");
 }
---
+++ opgaver-kap-8/8.3.4/Program.cs
@@ -0,0 +1,5 @@
+﻿double sum (double x, double y){
+    return x + y;
+}
+
+Console.Write(sum(5.6,4.3));
