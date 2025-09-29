804da37c4a35fa0abcb9be142a1d307d59b95fae
jeppe-dp <jeppedeleuran@yahoo.dk>
2025-09-29 16:01:20 +0200

    x

---
+++ opgaver-kap-9/9.1.2/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] accounts= {903, 716, 67};
+int GetAccountNumber ()
+{
+    Console.WriteLine("Enter an account number: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+void PrintAccountState(int accountId)
+{
+    Console.WriteLine("Account "+ accountId+ " contains "+ accounts[accountId]);
+}
+
+while(true) {
+    try {
+        int accountId = GetAccountNumber();
+        PrintAccountState(accountId);
+        break;
+    }
+    catch {
+        Console.WriteLine("Invalid account number");
+    }
+}
